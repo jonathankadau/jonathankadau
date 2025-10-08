365fee5148957523693df968b3fadec96c01f61e
jonathankadau <jjkadau49@gmail.com>
2025-10-08 16:55:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const projects = document.querySelectorAll('.project');
 const preview = document.getElementById('preview');
 
 projects.forEach(p => {
+  // Hover: show preview image
   p.addEventListener('mousemove', (e) => {
     const imgSrc = p.dataset.img;
     preview.src = imgSrc;
@@ -11,11 +12,13 @@ projects.forEach(p => {
     preview.classList.remove('hidden');
   });
 
+  // Move image with cursor
   p.addEventListener('mouseleave', () => {
     preview.style.opacity = 0;
     setTimeout(() => preview.classList.add('hidden'), 200);
   });
 
+  // Click: go to project page
   p.addEventListener('click', () => {
     const url = p.dataset.url;
     window.location.href = url;
