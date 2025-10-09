c14ef6021e0ff53bc85721a6ab010c93cfad61d0
jonathankadau <jjkadau49@gmail.com>
2025-10-08 19:22:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ const projects = document.querySelectorAll('.project');
 const preview = document.getElementById('preview');
 
 projects.forEach(p => {
-  // Show image preview near cursor
+  // Hover image preview
   p.addEventListener('mousemove', (e) => {
     const imgSrc = p.dataset.img;
     preview.src = imgSrc;
-    preview.style.left = e.pageX + 20 + 'px';
-    preview.style.top = e.pageY + 20 + 'px';
+    preview.style.left = (e.pageX + 20) + 'px';
+    preview.style.top = (e.pageY + 20) + 'px';
     preview.style.opacity = 1;
     preview.classList.remove('hidden');
   });
 
-  // Hide preview on mouse leave
+  // Hide image when leaving
   p.addEventListener('mouseleave', () => {
     preview.style.opacity = 0;
     setTimeout(() => preview.classList.add('hidden'), 200);
@@ -20,7 +20,6 @@ projects.forEach(p => {
 
   // Click: navigate to project page
   p.addEventListener('click', () => {
-    const url = p.dataset.url;
-    window.location.href = url;
+    window.location.href = p.dataset.url;
   });
 });
